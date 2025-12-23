71fb73476f4d8c827d6d1e71cb16f001f0e8f8cc
Abdullah Ramadan Ali <abdullah.ramadanali@gmail.com>
2025-12-23 11:49:02 +0200

    Create Generic Response

---
+++ Application/Bases Response/DataResponse.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+
+namespace Application.Bases_Response
+{
+    public class DataResponse<T>
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        public DataResponse()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        {
+
+        }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        public DataResponse(T data, string message)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        {
+            Successded = true;
+            Message = message;
+            Data = data;
+        }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        public DataResponse(string message)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        {
+            Successded = false;
+            Message = message;
+
+        }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        public DataResponse(string message, bool successded)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        {
+            Successded = successded;
+            Message = message;
+        }
+        public HttpStatusCode StatusCode { get; set; }
+        public object? Meta { get; set; }
+        public bool Successded { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+        public T Data { get; set; }
+    }
+
+}
